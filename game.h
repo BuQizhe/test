2df26371098424711d726d0d289965d6d9877e52
BuQizhe <buqizhe999@gmail.com>
2024-08-01 10:54:40 +0800

    Add files via upload

---
+++ game.h
@@ -0,0 +1,29 @@
+﻿//⽂件中写游戏需要的数据类型和函数声明等
+#pragma once
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+#define EASY_COUNT 10
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#define ROW 9
+#define COL 9
+
+#define ROWS ROW+2
+#define COLS COL+2
+
+//初始化棋盘
+void InitBoard(char arr[ROWS][COLS], int rows, int cols, char set);
+
+//打印棋盘
+void DisplayBoard(char arr[ROWS][COLS],int row,int col);
+
+//布置雷
+void SetMine(char board[ROWS][COLS], int row, int col);
+
+//排查雷
+void FindMine(char mine[ROWS][COLS], char show[ROWS][COLS], int row, int col);
+
