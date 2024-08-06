cd15a562bd477f558c37aba83faa25200c61bc84
BuQizhe <buqizhe999@gmail.com>
2024-08-06 21:25:34 +0800

    Add files via upload
    贪心算法---上楼梯

--- test.cpp
+++ test.cpp
@@ -1,27 +1,30 @@
-class  A
-{
-public:
-	virtual void show()
-	{
-		cout << "A show" << endl;
-	}
-};
-class B : public A
-{
-public:
-	void show()
-	{
-		cout << "B show" << endl;
-	}
-};
-int main()
-{
+//贪心算法上楼梯
 
-	B b;
-	b.show();//B show
-	A* pA = &b;
-	pA->show();//B show 如果show方法前没用virtual声明为虚函数，这里会输出A show
-
-	system("pause");
-	return 0;
+#include<stdio.h>
+// 使用贪心算法计算上楼梯的最少步数
+void greedyStairs(int n) {
+    int steps = 0; // 总步数
+    int threeSteps = 0; // 走3步的次数
+    int twoSteps = 0; // 走2步的次数
+    int oneStep = 0; // 走1步的次数
+    // 首先尽可能多地走3步
+    threeSteps = n / 3;
+    n -= threeSteps * 3;
+    // 然后尽可能多地走2步
+    twoSteps = n / 2;
+    n -= twoSteps * 2;
+    // 最后走剩下的1步
+    oneStep = n;
+    // 输出结果
+    printf("走3步的次数: %d\n", threeSteps);
+    printf("走2步的次数: %d\n", twoSteps);
+    printf("走1步的次数: %d\n", oneStep);
+    printf("总步数: %d\n", threeSteps + twoSteps + oneStep);
+}
+int main() {
+    int n;
+    printf("请输入楼梯的总步数: ");
+    scanf_s("%d", &n);
+    greedyStairs(n);
+    return 0;
 }
