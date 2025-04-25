f7605db9350b84897c2c4d87bb55eefebed14c88
BuQizhe <buqizhe999@gmail.com>
2025-04-25 21:44:56 +0800

    代码提交

--- test.cpp
+++ test.cpp
@@ -1,133 +1,82 @@
 
 
 
-//1.两数之和 C++
-//暴力求解
+//1534. 统计好三元组
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        int n = nums.size();
+    int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+        int n = arr.size(), cnt = 0;
         for (int i = 0; i < n; ++i) {
             for (int j = i + 1; j < n; ++j) {
-                if (nums[i] + nums[j] == target) {
-                    return { i, j };
+                for (int k = j + 1; k < n; ++k) {
+                    if (abs(arr[i] - arr[j]) <= a && abs(arr[j] - arr[k]) <= b && abs(arr[i] - arr[k]) <= c) {
+                        ++cnt;
+                    }
                 }
             }
         }
-        return {};
+        return cnt;
     }
 };
 
-//哈希表
+
+//584. 寻找用户推荐人
+// Write your MySQL query statement below
+SELECT name FROM customer WHERE referee_id != 2 OR referee_id IS NULL;
+
+
+//1757. 可回收且低脂的产品
+SELECT
+product_id
+FROM
+Products
+WHERE
+low_fats = 'Y' AND recyclable = 'Y'
+
+//709. 转换成小写字母
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> hashtable;
-        for (int i = 0; i < nums.size(); ++i) {
-            auto it = hashtable.find(target - nums[i]);
-            if (it != hashtable.end()) {
-                return { it->second, i };
+    string toLowerCase(string s) {
+        for (char& ch : s) {
+            if (ch >= 65 && ch <= 90) {
+                ch |= 32;
             }
-            hashtable[nums[i]] = i;
         }
-        return {};
+        return s;
     }
 };
 
-//26.删除有序数组的重复项 C++
-
-//双指针
 class Solution {
 public:
-    int removeDuplicates(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 0) {
-            return 0;
+    string toLowerCase(string s) {
+        for (char& ch : s) {
+            ch = tolower(ch);
         }
-        int fast = 1, slow = 1;
-        while (fast < n) {
-            if (nums[fast] != nums[fast - 1]) {
-                nums[slow] = nums[fast];
-                ++slow;
+        return s;
+    }
+};
+
+
+//258. 各位相加
+
+class Solution {
+public:
+    int addDigits(int num) {
+        while (num >= 10) {
+            int sum = 0;
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
             }
-            ++fast;
+            num = sum;
         }
-        return slow;
-    }
-};
-
-
-int removeDuplicates(vector<int>& nums) {
-    int length = nums.size();
-    int ptr = 0;
-
-    for (int i = 0; i < length; i++) {
-        if (nums[i] != nums[ptr]) {
-            ptr++;
-            nums[ptr] = nums[i];
-        }
-    }
-
-    return ptr + 1;
-}
-
-int removeDuplicates(int* nums, int numsSize) {
-    int j = 0;
-    for (int i = 1; i < numsSize; i++)
-    {
-        if (nums[i] != nums[j])
-        {
-            j++;
-            nums[j] = nums[i];
-        }
-    }
-    return j + 1;
-}
-
-
-//35.搜索插入位置 C++
-//二分查找
-
-class Solution {
-public:
-    int searchInsert(vector<int>& nums, int target) {
-        int n = nums.size();
-        int l = 0, r = n - 1;
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (nums[mid] < target)
-                l = mid + 1;
-            else r = mid - 1;
-        }
-        return l;
+        return num;
     }
 };
 
 class Solution {
 public:
-    int searchInsert(vector<int>& nums, int target) {
-        int n = nums.size();
-        int l = 0, r = n - 1;
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (nums[mid] < target)
-                l = mid + 1;
-            else r = mid - 1;
-        }
-        return l;
+    int addDigits(int num) {
+        return (num - 1) % 9 + 1;
     }
 };
-
-int searchInsert(int* nums, int numsSize, int target) {
-    int left = 0, right = numsSize - 1;
-    while (left <= right) {
-        int mid = (left + right) / 2;
-        if (nums[mid] < target) {
-            left = mid + 1;
-        }
-        else {
-            right = mid - 1;
-        }
-    }
-    return left;
-}
