fa63d042b1d1c406ed1f1ac5171ed01d08a4ecc9
BuQizhe <buqizhe999@gmail.com>
2025-04-28 20:49:59 +0800

    代码提交-test-25-4-28

--- test.cpp
+++ test.cpp
@@ -1,82 +1,66 @@
 
-
-
-//1534. 统计好三元组
+//852. 山脉数组的峰顶索引
 class Solution {
 public:
-    int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
-        int n = arr.size(), cnt = 0;
-        for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j < n; ++j) {
-                for (int k = j + 1; k < n; ++k) {
-                    if (abs(arr[i] - arr[j]) <= a && abs(arr[j] - arr[k]) <= b && abs(arr[i] - arr[k]) <= c) {
-                        ++cnt;
-                    }
-                }
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int n = arr.size();
+        int ans = -1;
+        for (int i = 1; i < n - 1; ++i) {
+            if (arr[i] > arr[i + 1]) {
+                ans = i;
+                break;
             }
         }
-        return cnt;
+        return ans;
     }
 };
 
-
-//584. 寻找用户推荐人
-// Write your MySQL query statement below
-SELECT name FROM customer WHERE referee_id != 2 OR referee_id IS NULL;
-
-
-//1757. 可回收且低脂的产品
-SELECT
-product_id
-FROM
-Products
-WHERE
-low_fats = 'Y' AND recyclable = 'Y'
-
-//709. 转换成小写字母
 class Solution {
 public:
-    string toLowerCase(string s) {
-        for (char& ch : s) {
-            if (ch >= 65 && ch <= 90) {
-                ch |= 32;
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int n = arr.size();
+        int left = 1, right = n - 2, ans = 0;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] > arr[mid + 1]) {
+                ans = mid;
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
             }
         }
-        return s;
+        return ans;
     }
 };
 
+//1470. 重新排列数组
 class Solution {
 public:
-    string toLowerCase(string s) {
-        for (char& ch : s) {
-            ch = tolower(ch);
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> ans(2 * n);
+        for (int i = 0; i < n; i++) {
+            ans[2 * i] = nums[i];
+            ans[2 * i + 1] = nums[i + n];
         }
-        return s;
+        return ans;
     }
 };
 
-
-//258. 各位相加
-
+//263. 丑数 就是只包含质因数 2、3 和 5 的 正 整数。 
+//给你一个整数 n ，请你判断 n 是否为 丑数 。如果是，返回 true ；否则，返回 false 。
 class Solution {
 public:
-    int addDigits(int num) {
-        while (num >= 10) {
-            int sum = 0;
-            while (num > 0) {
-                sum += num % 10;
-                num /= 10;
+    bool isUgly(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        vector<int> factors = { 2, 3, 5 };
+        for (int factor : factors) {
+            while (n % factor == 0) {
+                n /= factor;
             }
-            num = sum;
         }
-        return num;
-    }
-};
-
-class Solution {
-public:
-    int addDigits(int num) {
-        return (num - 1) % 9 + 1;
+        return n == 1;
     }
 };
